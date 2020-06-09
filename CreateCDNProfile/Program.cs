fd1f87bd706641b3da6138d8f89304bdce2f250b
chenzheng <czhsn1988@163.com>
2020-06-09 09:34:01 +0000

    update

--- CreateCDNProfile/Program.cs
+++ CreateCDNProfile/Program.cs
@@ -12,12 +12,12 @@ namespace CreateCDNProfile
 {
     class Program
     {
-        static string clientId = "c6764c13-4613-4725-89de-b64ccd9d6c45";
-        static string clientSecret = "Di:B]B5653IebJCTI?bfRh-TBPh/H-Xn";
-        static string tenantID = "b388b808-0ec9-4a09-a414-a7cbbd8b7e9b";
+        static string clientId = "Your-Client-ID";
+        static string clientSecret = "Your-Client-Secret";
+        static string tenantID = "Your-TenantID";
         static void Main(string[] args)
         {
-            string url = "https://management.chinacloudapi.cn/subscriptions/86d19745-1e57-4bdf-be8d-c17e3792c594/resourceGroups/testgroup1/providers/Microsoft.Cdn/profiles/myprofile3?api-version=2019-04-15";
+            string url = "https://management.chinacloudapi.cn/subscriptions/Your-SubID/resourceGroups/Your-esource-Group-Name/providers/Microsoft.Cdn/profiles/Your-CDN-Profile-Name?api-version=2019-04-15";
             string cdnjson = "{\r\n  \"location\": \"Chinanorth\",\r\n  \"sku\": {\r\n    \"name\": \"Premium_ChinaCdn\"\r\n  }\r\n}";
             PutCreateCDNProfile(url,cdnjson);
             // The code provided will print ‘Hello World’ to the console.
@@ -32,7 +32,7 @@ namespace CreateCDNProfile
             try
             {
                 AuthenticationContext authContext =
-                       new AuthenticationContext("https://login.chinacloudapi.cn/b388b808-0ec9-4a09-a414-a7cbbd8b7e9b");
+                       new AuthenticationContext("https://login.chinacloudapi.cn/Your-TenantID");
                 ClientCredential credential = new ClientCredential(clientId, clientSecret);
                 AuthenticationResult result =
                  authContext.AcquireTokenAsync("https://management.chinacloudapi.cn/", credential).GetAwaiter().GetResult();
@@ -43,23 +43,6 @@ namespace CreateCDNProfile
                 return "";
             }
         }
-        //public static string CreateCDNProfile()
-        //{
-        //    var token = gettoken();
-        //    var api = "https://management.chinacloudapi.cn/subscriptions/86d19745-1e57-4bdf-be8d-c17e3792c594/resourceGroups/testgroup1/providers/Microsoft.Cdn/profiles/myprofile1?api-version=2019-04-15?api-version=2019-05-10";
-        //    var http = new HttpClient();
-        //    var req = new HttpRequestMessage(HttpMethod.Get, api);
-        //    req.Headers.Add("Authorization", "Bearer " + token);
-        //    var resp = http.SendAsync(req).Result;
-        //    if (resp.IsSuccessStatusCode)
-        //    {
-        //        var json = JsonConvert.DeserializeObject<JObject>(resp.Content.ReadAsStringAsync().Result);
-
-        //        return json["value"].ToString();
-        //    }
-        //    return "";
-
-        //}
         
         public static string PutCreateCDNProfile(string url, string cdnjson)
         {
@@ -82,58 +65,6 @@ namespace CreateCDNProfile
             }
             return "failed";
         }
-        //private static string GetToken()
-        //{
-        //    try
-        //    {
-        //        var client = new HttpClient();
-        //        var request = new HttpRequestMessage(HttpMethod.Post, "https://login.chinacloudapi.cn/tentid/oauth2/token?api-version=1.0");
-        //        var dictionary = new Dictionary<string, string>
-        //    {
-        //        {"resource","https://microsoftgraph.chinacloudapi.cn/" },
-        //        {"client_id","值" },
-        //        {"grant_type","client_credentials" },
-        //        {"client_secret","值" }
-
-        //    };
-        //        request.Content = new FormUrlEncodedContent(dictionary);
-        //        var result = client.SendAsync(request).Result;
-        //        if (result.IsSuccessStatusCode)
-        //        {
-        //            var json = JsonConvert.DeserializeObject<JObject>(result.Content.ReadAsStringAsync().Result);
-        //            return json["access_token"].ToString();
-        //        }
-        //        return string.Empty;
-        //    }
-        //    catch
-        //    {
-        //        return string.Empty;
-        //    }
-        //}
-
-        //public static async Task<string> PostAsyncJson(string url, string json)
-        //{
-        //    HttpClient client = new HttpClient();
-        //    HttpContent content = new StringContent(json);
-        //    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-        //    HttpResponseMessage response = await client.PostAsync(url, content);
-        //    response.EnsureSuccessStatusCode();
-        //    string responseBody = await response.Content.ReadAsStringAsync();
-        //    return responseBody;
-        //}
-        //public static string GetAccessTokenAsync()
-        //{
-        //    AuthenticationContext context = new
-        //    AuthenticationContext(
-        //        string.Format("https://login.chinacloudapi.cn/b388b808-0ec9-4a09-a414-a7cbbd8b7e9b/oauth2/token?api-version=1.0"));
-        //    var credential = new ClientCredential("4ab4cad5-e3b4-4b13-8a43-3dd88be080aa", "R:b/3?4AJ3rQ@6wLjms8ch.8w3J=OkIC");
-        //    AuthenticationResult authenticationResult =
-        //    context.AcquireTokenAsync(
-        //        "https://management.chinacloudapi.cn/",
-        //        credential).GetAwaiter().GetResult();
-        //    return authenticationResult.AccessToken;
-        //}
-
-
+       
     }
 }
